6290e9d4f351e6c66c2bd60def0fcebe8ed98c33
Huy-DNA <huydo862003@gmail.com>
2024-10-07 20:36:38 +0700

    feat: basic terminal bg

---
+++ components/Terminal/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-3 flex gap-2">
+    <div class="w-2 h-2 bg-red rounded-full" />
+    <div class="w-2 h-2 bg-yellow rounded-full" />
+    <div class="w-2 h-2 bg-green rounded-full" />
+    <div class="w-2 h-2 bg-cyan rounded-full" />
+    <div class="w-2 h-2 bg-blue rounded-full" />
+    <div class="w-2 h-2 bg-pink rounded-full" />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,15 @@
 </script>
 
 <template>
-  <main class="w-[100vw] h-[100vh] bg-background text-white">
+  <main class="terminal w-[100vw] h-[100vh] bg-background text-white p-5">
+    <TerminalHeader />
+    <TerminalContent />
   </main>
 </template>
+
+<style>
+  .terminal * {
+    white-space: pre;
+    font-family: NerdFontProto, 'Courier New', Courier, monospace;
+  }
+</style>

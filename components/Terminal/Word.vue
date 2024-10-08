e25535711dd9173bf814afbe543e374c71ce6b6a
Huy-DNA <huydo862003@gmail.com>
2024-10-08 13:42:03 +0700

    fix: avoid double insertion

--- components/Terminal/Word.vue
+++ components/Terminal/Word.vue
@@ -7,20 +7,29 @@
     input: [word: string];
   }>();
 
+  const inputRef = useTemplateRef('input');
+
   function onInput (e: InputEvent) {
     emits('input', (e.target as any).innerText);
+    (e.target as any).innerText = '';
   }
 </script>
 
 <template>
   <span
     v-if="props.editable || false"
-    :class="`text-${props.word.color} outline-none`" contenteditable @input="onInput">
+    :class="`text-${props.word.color} outline-none`"
+    contenteditable
+    ref="input"
+    @input="onInput"
+  >
     {{ props.word.content }}
   </span>
   <span
     v-else
-    :class="`text-${props.word.color}`" contenteditable>
+    :class="`text-${props.word.color}`"
+    contenteditable
+  >
     {{ props.word.content }}
   </span>
 </template>

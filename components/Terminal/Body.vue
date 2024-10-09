3ac153eecc0290c3ebfbb64fab5861dead5e2962
Huy-DNA <huydo862003@gmail.com>
2024-10-09 19:20:06 +0700

    chore: init editableLine component

--- components/Terminal/Body.vue
+++ components/Terminal/Body.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
   const content: Ref<TContent> = ref([]);
-  const currentLine: Ref<TLine> = ref([]);
+  const currentLine: Ref<string> = ref('');
 </script>
 
 <template>
   <div class="pl-2">
     <TerminalContent :content="content" />
+    <TerminalEditableLine :content="currentLine" />
   </div>
 </template>
---
+++ components/Terminal/EditableLine.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const props = defineProps<{ content: string }>();
+</script>
+
+<template>
+  <p role="text" class="flex justify-start gap-0 w-[100%]">
+  </p>
+</template>

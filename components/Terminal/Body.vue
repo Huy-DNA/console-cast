bf9507341df7526dcacd75d54867ac6515aa0487
Huy-DNA <huydo862003@gmail.com>
2024-10-09 23:55:24 +0700

    feat: place cursor based on position

--- components/Terminal/Body.vue
+++ components/Terminal/Body.vue
@@ -6,14 +6,21 @@
   const editableLine = ref(null);
   
   const lineCount = computed(() => content.value.length + 1);
-  const lineNodes = computed(() => [...(nonEditableLines.value || []).map(({ root }) => root), editableLine.value.root]);
-  const cursorPositionInViewport = computed(() => {
+  const lineNodes = computed(() => {
+    if (!editableLine.value) return [];
+    return [...(nonEditableLines.value || []).map(({ root }) => root), editableLine.value.root];
+  });
+
+  watch([cursorPosition, lineNodes], () => {
     const { line, offset } = cursorPosition.value;
     const lineNode = lineNodes.value[line];
-    return getCharPosition(lineNode, offset);
+    const position = lineNode ? getCharPosition(lineNode, offset) : { top: 0, left: 0 };
+    const cursor = document.getElementById('cursor');
+    cursor!.style.top = `${position.top}px`;
+    cursor!.style.left = `${position.left}px`;
   });
  
-  function getCharPosition (element: Element, offset: number) {
+  function getCharPosition (element: Element, offset: number): { top: number, left: number } {
     let characterCount = 0;
     const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
     let node;
@@ -28,12 +35,15 @@
         const rect = range.getBoundingClientRect(); 
         return { top: rect.top, left: rect.left };
       }
+      characterCount += textLength;
     }
+    return { top: 0, left: 0 };
   };
 </script>
 
 <template>
   <div class="pl-2 caret-transparent">
+    <div class="w-2 h-[22px] absolute bg-white z-50" id="cursor" />
     <TerminalLine v-for="(line, index) in content" :key="index" :line="line" ref="nonEditableLines" />
     <TerminalEditableLine :content="currentLine" ref="editableLine" />
   </div>

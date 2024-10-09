514fb3d18ce9f146e796a885d499e303beccec4b
Huy-DNA <huydo862003@gmail.com>
2024-10-09 19:57:26 +0700

    feat: render words in editable lines

--- components/Terminal/EditableLine.vue
+++ components/Terminal/EditableLine.vue
@@ -4,9 +4,21 @@
   const emits = defineEmits<{
     keypress: [key: string, offset: number],
   }>();
+
+  const words = computed(() => {
+    return props.content.split(/(\s+)/);
+  });
+
+  const coloredWords = computed(() => {
+    return words.value.map((word) => ({
+      content: word,
+      color: TColor.WHITE,
+    }));
+  });
 </script>
 
 <template>
   <p role="text" class="flex justify-start gap-0 w-[100%]">
+    <TerminalWord v-for="(word, index) in coloredWords" :key="index" :word="word" />
   </p>
 </template>

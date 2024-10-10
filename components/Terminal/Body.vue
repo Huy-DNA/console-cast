25687d5a156b9c034cece9cab9de98dd2a2d7eea
Huy-DNA <huydo862003@gmail.com>
2024-10-11 01:27:57 +0700

    fix: improve command navigation

--- components/Terminal/Body.vue
+++ components/Terminal/Body.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+  const historyCommands: Ref<TContent> = ref([]);
   const previousLines: Ref<TContent> = ref([]);
   const currentLine = ref('');
-  const curLineIndex = ref(0);
-  const lineCount = computed(() => previousLines.value.length + 1);
+  const curCommandIndex = ref(0);
+  const commandCount = computed(() => historyCommands.value.length + 1);
 
   const editableLine = ref(null);
 
@@ -13,10 +14,11 @@
 
   async function onSubmit (line: TLine) {
     currentLine.value = '';
+    if (line.length > 1 || line[0].content.trim()) historyCommands.value.push(line);
     previousLines.value.push(line);
-    curLineIndex.value = lineCount.value - 1;
     const executeResult = await execute(line.map(({ content }) => content));
     previousLines.value.push(...executeResult);
+    curCommandIndex.value = commandCount.value - 1;
     await nextTick();
     editableLine.value.updateCursor();
   }
@@ -30,15 +32,19 @@
   }
 
   function onLineUp () {
-    if (curLineIndex.value <= 0) return;
-    curLineIndex.value -= 1;
-    currentLine.value = previousLines.value[curLineIndex.value].map(({ content }) => content).join('');
+    if (curCommandIndex.value <= 0) return;
+    curCommandIndex.value -= 1;
+    currentLine.value = historyCommands.value[curCommandIndex.value].map(({ content }) => content).join('');
   }
 
-  function onLineDown () {
-    if (curLineIndex.value >= lineCount.value - 2) return;
-    curLineIndex.value += 1;
-    currentLine.value = previousLines.value[curLineIndex.value].map(({ content }) => content).join('');
+  function onLineDown () { 
+    if (curCommandIndex.value > commandCount.value - 2) return;
+    curCommandIndex.value += 1;
+    if (curCommandIndex.value === commandCount.value - 2) {
+      currentLine.value = '';
+    } else {
+      currentLine.value = historyCommands.value[curCommandIndex.value].map(({ content }) => content).join('');
+    }
   }
 </script>
 

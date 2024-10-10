de9bfcebcd9309c0a9a490bf74d7213892229201
Huy-DNA <huydo862003@gmail.com>
2024-10-11 00:37:18 +0700

    feat: highlight color as pink

--- utils/command/highlight.ts
+++ utils/command/highlight.ts
@@ -5,6 +5,9 @@ export function highlight (args: string[]): TWord[] {
       return { content: arg, color: TColor.WHITE };
     }
     nonspaceArgIndex += 1;
+    if (arg.match(/^[0-9]+$/)) {
+      return { content: arg, color: TColor.PINK };
+    }
     if (["\'", "\""].includes(arg[0])) {
       return { content: arg, color: TColor.CYAN };
     }

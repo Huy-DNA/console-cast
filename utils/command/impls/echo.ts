7b117415d0413e5bc39c225218a0192fe2119896
Qmi <minh.pham1126@gmail.com>
2024-10-11 15:20:43 +0700

    fix(echo): shift first space after echo

--- utils/command/impls/echo.ts
+++ utils/command/impls/echo.ts
@@ -1,28 +1,31 @@
-import type { TCommand } from './types';
+import type { TCommand } from "./types";
 
 const colorCodes = {
-  '\u001b[30m': TColor.BLACK,
-  '\u001b[31m': TColor.RED,
-  '\u001b[32m': TColor.GREEN,
-  '\u001b[33m': TColor.YELLOW,
-  '\u001b[34m': TColor.BLUE,
-  '\u001b[35m': TColor.PURPLE,
-  '\u001b[36m': TColor.CYAN,
-  '\u001b[37m': TColor.WHITE,
-  '\u001b[95m': TColor.PINK,
+  "\u001b[30m": TColor.BLACK,
+  "\u001b[31m": TColor.RED,
+  "\u001b[32m": TColor.GREEN,
+  "\u001b[33m": TColor.YELLOW,
+  "\u001b[34m": TColor.BLUE,
+  "\u001b[35m": TColor.PURPLE,
+  "\u001b[36m": TColor.CYAN,
+  "\u001b[37m": TColor.WHITE,
+  "\u001b[95m": TColor.PINK,
 };
 
-export const echo: TCommand = function (...args) {
+export const echo: TCommand = function(...args) {
   args.shift();
-  return [args.flatMap((arg) => {
-    if (!arg.trim()) return [{ content: ' ', color: TColor.WHITE }];
-    const chunks = formatArg(arg).split(/(\u001b\[[0-9]*m)/);
-    const res = [{ content: chunks.shift()!, color: TColor.WHITE }];
-    while (chunks.length) {
-      const color = (colorCodes as any)[chunks.shift()!] || TColor.WHITE;
-      const text = chunks.shift()!;
-      res.push({ content: text, color });
-    }
-    return res;
-  })];
-}
+  args.shift();
+  return [
+    args.flatMap((arg) => {
+      if (!arg.trim()) return [{ content: " ", color: TColor.WHITE }];
+      const chunks = formatArg(arg).split(/(\u001b\[[0-9]*m)/);
+      const res = [{ content: chunks.shift()!, color: TColor.WHITE }];
+      while (chunks.length) {
+        const color = (colorCodes as any)[chunks.shift()!] || TColor.WHITE;
+        const text = chunks.shift()!;
+        res.push({ content: text, color });
+      }
+      return res;
+    }),
+  ];
+};

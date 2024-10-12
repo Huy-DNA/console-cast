d51639bd492ce5769b6d3112c5bfda890c3347ad
Huy-DNA <huydo862003@gmail.com>
2024-10-12 18:16:15 +0700

    fix: swap name of groups and users services

--- services/groups.ts
+++ services/groups.ts
@@ -1,17 +1,12 @@
 import type { Diagnostic, Result } from './types';
 
-export interface UserMeta {
+export interface GroupMeta {
   name: string;
-  userId: number;
   groupId: number;
   createdAt: Date;
 }
 
-export const userService = {
-  async getMetaOfUser (id: number): Promise<Result<UserMeta, Diagnostic>> {
-  },
-  async getHomeDirectory (id: number): Promise<Result<string, Diagnostic>> {
-  },
-  async switchUser (name: string, password: string): Promise<Result<UserMeta, Diagnostic>> {
+export const groupService = {
+  async getMetaOfGroup (id: number): Promise<Result<GroupMeta, Diagnostic>> {
   },
 };
--- services/users.ts
+++ services/users.ts
@@ -1,12 +1,17 @@
 import type { Diagnostic, Result } from './types';
 
-export interface GroupMeta {
+export interface UserMeta {
   name: string;
+  userId: number;
   groupId: number;
   createdAt: Date;
 }
 
-export const groupService = {
-  async getMetaOfGroup (id: number): Promise<Result<GroupMeta, Diagnostic>> {
+export const userService = {
+  async getMetaOfUser (id: number): Promise<Result<UserMeta, Diagnostic>> {
+  },
+  async getHomeDirectory (id: number): Promise<Result<string, Diagnostic>> {
+  },
+  async switchUser (name: string, password: string): Promise<Result<UserMeta, Diagnostic>> {
   },
 };

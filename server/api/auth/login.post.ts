81314837c8b532daa5a49ea53900a13dee79f871
Huy-DNA <huydo862003@gmail.com>
2024-10-12 21:45:33 +0700

    fix: include userid in the login jwt

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -18,11 +18,11 @@ export default defineEventHandler(async (event) => {
   }
   const { name, password } = body;
   
-  const { password: hashedPassword } = await db.selectExactlyOne('users', { name }).run(dbPool);
+  const { password: hashedPassword, id } = await db.selectExactlyOne('users', { name }).run(dbPool);
   
   if (hashedPassword === null || (password && await bcrypt.compare(password, hashedPassword))) {
     const { JWT_SECRET } = useRuntimeConfig();
-    const token = jwt.sign({ username: name }, JWT_SECRET, { expiresIn: '24h' });
+    const token = jwt.sign({ username: name, userid: id }, JWT_SECRET, { expiresIn: '24h' });
     setHeader(event, 'Set-Cookie', `jwt=${token}; HttpOnly; SameSite=Strict${isProduction ? '' : '; Secure'}`);
     return { ok: { message: 'Login successfully' } };
   }

62651396787f04145ce6add6364e2888c8b8753e
Huy-DNA <huydo862003@gmail.com>
2024-10-14 23:44:33 +0700

    feat: delete user api

---
+++ server/api/users/index.delete.ts
@@ -0,0 +1,32 @@
+import { formatArg } from "~/lib/command/utils";
+import * as db from 'zapatos/db';
+import { dbPool } from '~/db/connection';
+
+export enum UserDeleteErrorCode {
+  INVALID_PARAM = 1000,
+  UNDELETABLE_USER = 1001,
+  NOT_ENOUGH_PRIVILEGE = 2000,
+}
+
+export default defineEventHandler(async (event) => {
+  const { name } = getQuery(event);
+  if (typeof name !== 'string') {
+    return { error: { code: UserDeleteErrorCode.INVALID_PARAM, message: 'Expect the "name" query param to be string' } };
+  }
+  if (!event.context.auth) {
+    return { error: { code: UserDeleteErrorCode.NOT_ENOUGH_PRIVILEGE, message: 'Should be logged in as a user with enough privilege' } };
+  }
+  const formattedName = formatArg(name);
+  // Only allow a user to remove itself currently
+  if (formattedName !== event.context.auth.username) {
+    return { error: { code: UserDeleteErrorCode.NOT_ENOUGH_PRIVILEGE, message: 'Should be logged in as a user with enough privilege' } };
+  }
+  // root and guest can be deleted
+  if (['root', 'guest'].includes(formattedName)) {
+    return { error: { code: UserDeleteErrorCode.UNDELETABLE_USER, message: `User "${formattedName}" cannot be delete` } };
+  }
+
+  await db.update('users', { name: formattedName }, { deleted_at: new Date(Date.now()) }).run(dbPool);
+  
+  return { ok: { message: 'Delete user successfully' } };
+});

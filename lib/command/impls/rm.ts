4b98b2756891e29f28970c0f4a3800d0d9a545e8
Huy-DNA <huydo862003@gmail.com>
2024-11-05 12:43:34 +0700

    feat: basic rm

--- lib/command/impls/rm.ts
+++ lib/command/impls/rm.ts
@@ -1,3 +1,4 @@
+import { uniq } from 'lodash-es';
 import { fileService } from '~/services';
 import { formatArg } from '../utils';
 import type { CommandFunc } from './types';
@@ -8,5 +9,15 @@ export const rm: CommandFunc = async function(...args) {
   // discard first space
   args.shift();
 
-  return [];
+  const { cwd } = useCwdStore();
+  const filenames = uniq(args.filter((arg) => arg.trim()).map((arg) => cwd.value.resolve(formatArg(arg)).toString()));
+
+  const lines = [];
+  for (const filename of filenames) {
+    const res = await fileService.removeFile(filename);
+    if (!res.isOk()) {
+      lines.push(`${filename}: ${res.error()!.message}`);
+    }
+  }
+  return lines;
 };

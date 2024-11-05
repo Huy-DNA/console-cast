acbfefb3f037c53e1ce58d18ce595aeae0104c4b
Huy-DNA <huydo862003@gmail.com>
2024-11-05 11:26:19 +0700

    fix: change internal umask representation

--- composables/umask.ts
+++ composables/umask.ts
@@ -1,12 +1,12 @@
 import { createGlobalState } from '@vueuse/core';
 
 export const useUmaskStore = createGlobalState(() => {
-  const umask = ref(window?.localStorage?.getItem('umask') || '002');
-  function changeUmask(newUmask: string) {
+  const umask = ref(window?.localStorage?.getItem('umask') || '000000000010');
+  function changeUmask(newUmask: string & { length: 12 } & { [index: number]: '0' | '1' }) {
     umask.value = newUmask;
   }
   function clearUmask() {
-    umask.value = '002';
+    umask.value = '000000000010';
   }
   return {
     umask,

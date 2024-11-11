8c9123c3aabaf64f6633eadd4c3e729a9152ff58
Huy-DNA <huydo862003@gmail.com>
2024-11-11 23:12:33 +0700

    feat: call copyFile service in cp

--- lib/command/impls/cp.ts
+++ lib/command/impls/cp.ts
@@ -8,6 +8,18 @@ export const cp: CommandFunc = async function(...args) {
   // discard first space
   args.shift();
 
+  const src = formatArg(args.shift());
+  args.shift();
+  const dest = formatArg(args.shift());
+  args.shift();
+  if (args.length > 0 || !src || !dest) {
+    return [
+      'Invalid use of cp. Run \'help cp\'',
+    ];
+  }
+  const { umask } = useUmaskStore();
+  const res = await fileService.copyFile(src, dest, umask.value);
+
   return [
   ];
 };

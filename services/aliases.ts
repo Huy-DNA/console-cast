b2d033237854c37145c14bc2be2c6f50c4be1454
Huy-DNA <huydo862003@gmail.com>
2024-11-12 22:01:25 +0700

    fix: setAlias service

--- services/aliases.ts
+++ services/aliases.ts
@@ -2,22 +2,41 @@ import { AliasGetErrorCode } from '~/lib';
 import { Err, Ok, type Diagnostic, type Result } from './types';
 
 let allAliases: { name: string; command: string }[] | undefined = undefined;
+async function init () {
+  allAliases = (await $fetch('/api/aliases')).ok.data.commands;
+}
 
 export const aliasService = {
-  async init () {
-    allAliases = (await $fetch('/api/aliases')).ok.data.commands;
-  },
-  async getAlias(name: string): Promise<Result<string, Diagnostic>> {
-    if (!allAliases) await aliasService.init();
+  async getAlias (name: string): Promise<Result<string, Diagnostic>> {
+    if (!allAliases) await init();
     const alias = allAliases!.find(({ name: entryName }) => entryName === name);
     if (alias === undefined) {
       return new Err({ code: AliasGetErrorCode.ALIAS_NOT_FOUND, message: 'Alias not found' });
     }
     return new Ok(alias.command);
   },
-  async hasAlias(name: string): Promise<boolean> {
-    if (!allAliases) await aliasService.init();
+  async hasAlias (name: string): Promise<boolean> {
+    if (!allAliases) await init();
     const alias = allAliases!.find(({ name: entryName }) => entryName === name);
     return alias !== undefined;
-  }
+  },
+  async setAlias (name: string, command: string): Promise<Result<null, Diagnostic>> {
+    const res = await $fetch('/api/aliases', {
+      body: {
+        name,
+        command,
+      },
+      credentials: 'include',
+    });
+    if (res.error) {
+      return new Err({ code: res.error.code, message: res.error.message });
+    }
+    if (!allAliases) await init();
+    const alias = allAliases!.find(({ name: entryName }) => entryName === name);
+    if (alias) {
+      alias.command = command;
+    } else {
+      allAliases!.push({ name, command });
+    }
+  },
 };

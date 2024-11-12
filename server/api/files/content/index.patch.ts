af42ba220df300e6b7451e71c80df29b7c0452c1
Huy-DNA <huydo862003@gmail.com>
2024-11-12 18:44:19 +0700

    fix: remove unnecessary console.log

--- server/api/files/content/index.patch.ts
+++ server/api/files/content/index.patch.ts
@@ -36,8 +36,7 @@ export default defineEventHandler(async (event) => {
     await db.update('files', { content: newContent }, { name: filepath.toString(), deleted_at: db.conditions.isNull }).run(dbPool);
 
     return { ok: { message: 'Update file content successfully' } };
-  } catch (e) {
-    console.log(e);
+  } catch {
     return { error: { code: FileContentPatchErrorCode.FILE_NOT_FOUND, message: 'File not found' } };
   }
 });

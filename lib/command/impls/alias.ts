008bbe066713c32d8ea1f56e9aa4d37c7e367aae
Huy-DNA <huydo862003@gmail.com>
2024-11-12 22:13:29 +0700

    feat: basic alias command

--- lib/command/impls/alias.ts
+++ lib/command/impls/alias.ts
@@ -8,10 +8,24 @@ export const alias: AsyncCommandFunc = async function (...args) {
 
   if (args.length !== 1 || !args[0].trim()) {
     return [
-      'Invalid use of alias. Run \'help alias\'',
+      'Invalid alias expression, expected: <alias>=<command>',
     ];
   }
 
   const aliasExpr = formatArg(args[0])!;
-  return [];
+  const aliasTokens = aliasExpr.split('=');
+  if (aliasTokens.length <= 1) {
+    return [
+      'Invalid alias expression, expected: <alias>=<command>',
+    ];
+  }
+  const alias = formatArg(aliasTokens.shift()!.trim())!;
+  const command = formatArg(aliasTokens.join('=').trim())!;
+  const res = await aliasService.setAlias(alias, command);
+  if (res.isOk()) {
+    return [];
+  }
+  return [
+    res.error()!.message,
+  ];
 };

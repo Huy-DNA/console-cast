a212ec14836bbce7cd60f08c9037cdc80b721624
Huy-DNA <huydo862003@gmail.com>
2024-11-12 21:10:45 +0700

    fix: alias get api now allows fetch all

--- server/api/aliases/index.get.ts
+++ server/api/aliases/index.get.ts
@@ -4,16 +4,21 @@ import { dbPool } from '~/db/connection';
 
 export default defineEventHandler(async (event) => {
   const { name } = getQuery(event);
-  if (typeof name !== 'string') {
-    return { error: { code: AliasGetErrorCode.INVALID_PARAM, message: 'Expect the "name" query param to be string' } };
+  if (!['string', 'undefined'].includes(typeof name)) {
+    return { error: { code: AliasGetErrorCode.INVALID_PARAM, message: 'Expect the "name" query param to be an optional string' } };
   }
   if (!event.context.auth) {
     return { error: { code: AliasGetErrorCode.NOT_LOGIN, message: 'Should be logged in as a user with enough privilege' } };
   }
-  try {
-    const { command } = await db.selectExactlyOne('aliases', { name }).run(dbPool);
-    return { ok: { message: 'Fetch alias successfully', data: { command } } };
-  } catch {
-    return { error: { code: AliasGetErrorCode.ALIAS_NOT_FOUND, message: 'Alias not found' } };
+  if (name) {
+    try {
+      const { command } = await db.selectExactlyOne('aliases', { name: name as string, owner_id: Number.parseInt(event.context.auth.userId) }).run(dbPool);
+      return { ok: { message: 'Fetch alias successfully', data: { command } } };
+    } catch {
+      return { error: { code: AliasGetErrorCode.ALIAS_NOT_FOUND, message: 'Alias not found' } };
+    }
+  } else {
+    const commands = await db.select('aliases', { owner_id: Number.parseInt(event.context.auth.userId) }).run(dbPool);
+    return { ok: { message: 'Fetch aliases successfully', data: { commands } } };
   }
 });

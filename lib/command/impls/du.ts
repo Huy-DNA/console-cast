5ff0b3f580317b824022ce4c46ad6dbe87412529
Huy-DNA <huydo862003@gmail.com>
2024-11-12 22:57:40 +0700

    feat: extract pathname of du

--- lib/command/impls/du.ts
+++ lib/command/impls/du.ts
@@ -2,9 +2,17 @@ import { fileService } from '~/services';
 import { formatArg } from '../utils';
 import type { AsyncCommandFunc } from './types';
 
-export const du: AsyncCommandFunc = async function (...args) {
+export const du: AsyncCommandFunc = async function(...args) {
   // discard `du`
   args.shift();
+
+  if (args.length > 1) {
+    return [
+      'Invalid use of du. Run \'help du\'',
+    ];
+  }
+
+  const pathname = formatArg(args[0]) || '.';
 
   return [];
 };
